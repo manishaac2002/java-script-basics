814d398f460a04bf962e8a42d504f38520d5ce62
manishaac2002 <manishaac2002@gmail.com>
2023-06-21 13:45:51 +0100

    string methods

--- bascisOFJs/StringMethods.js
+++ bascisOFJs/StringMethods.js
@@ -1,6 +1,9 @@
-let firstName = "Manisha"
+let firstName = "This is my name Manisha"
 let lastName = "A C"
 
 // Concatenation
-let concatenation = firstName+" "+lastName
-console.log(concatenation);
+let concatenation = firstName+" "+ lastName
+console.log("Concatenation : "+ concatenation);
+
+concatenation = firstName.concat(' '+ lastName)
+console.log("concat : "+ concatenation);
