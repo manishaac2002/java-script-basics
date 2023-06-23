1141ffc03fd793aac9f08cfc899521c5ec56d531
manishaac2002 <manishaac2002@gmail.com>
2023-06-23 12:26:51 +0100

    chore--

--- oprators/strict.equality.js
+++ oprators/strict.equality.js
@@ -1,12 +1,13 @@
 // ----------------strict equality---------------//
 
-let num1=11;
-let num2=12
-console.log(typeof(num2));
-console.log(num1=num2);
-console.log(num1==num2);
+let num1 = 11;
+let num2 = '11'
+console.log(typeof (num2));
+console.log(num1 = num2);
+console.log(num1 == num2);
 // double equal to(==) returns the boolean type//
-console.log (num1==num2);
-console.log (typeof(num1==num2));//boolean 
+console.log(num1 == num2);
+console.log(typeof (num1 == num2));//boolean 
 
-console.log (num1===num2);
+console.log(num1 === num2);//it returns in boolean and consider datatype equality
+
