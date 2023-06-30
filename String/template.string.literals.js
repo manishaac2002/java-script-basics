9538097bd9e22a665ce20a063596c5b67daf1feb
manishaac2002 <manishaac2002@gmail.com>
2023-06-30 17:46:14 +0530

    template literals || arrays.

--- String/template.string.literals.js
+++ String/template.string.literals.js
@@ -2,3 +2,8 @@ let myName ="Manisha"
 let age ="21"
 let city ="Tirupur"
 let role ="Back End Dev"
+
+// template literal
+// ``-->back tick
+const output =  `I'm ${myName} and my age is ${age} . I'm living in city ${city} and my role is ${role}`  
+console.log(output);
---
+++ arrays/arrays.js
@@ -0,0 +1,5 @@
+// arrays
+let a=[12,21,23,34]
+console.log(a);
+console.table(a)//to view the index
+console.log(a[1])//to know the index value of the array element
