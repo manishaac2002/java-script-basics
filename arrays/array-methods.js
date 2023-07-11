f70b95e779befd419107c4beba57c0c2a6be1c72
manishaac2002 <manishaac2002@gmail.com>
2023-07-11 16:05:52 +0530

    string methods.

--- arrays/array-methods.js
+++ arrays/array-methods.js
@@ -33,8 +33,29 @@ console.log("Slice : "+myArray1.slice(2));//(start,end)
 console.log("Slice : "+myArray1.slice(2, 5));
 
 //----------------splice method-----------------
+//(start,length,new element)
+//return function -->which return the remove elements
 
 let a1 = [1, 2, 3, 4, 5, 6, 7,8,9,10];
-console.log("Splice : "+a1.splice(2));//(start,length,new element)
-console.log("Splice : "+a1.splice(2,2));
+console.log("Before Splice : "+a1);
+let removedElements =a1.splice(2)
+console.log("Removed Splice : "+removedElements);
+console.log("After Splice : "+a1);
+
+//passing two arg
+let a2 = [1, 2, 3, 4, 5, 6, 7,8,9,10];
+console.log("Before Splice : "+a2);
+removedElements =a2.splice(2,2)
+console.log("Removed Splice : "+removedElements);
+console.log("After Splice : "+a2);
+
+//three arg(new element)
+//removing the element and inserting the new element by passing third element
+let a3 = [1, 2, 3, 4, 5, 6, 7,8,9,10];
+console.log("Before Splice : "+a3);
+removedElements =a3.splice(2,2,35,89)
+console.log("Removed Splice : "+removedElements);
+console.log("After Splice : "+a3);
+
+
 
